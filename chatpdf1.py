e9a21dc77d052700916767bf51e0bd3bc349b34b
neemakhati <64632758+neemakhati@users.noreply.github.com>
2025-03-12 11:51:55 +0545

    Update chatpdf1.py

--- chatpdf1.py
+++ chatpdf1.py
@@ -90,7 +90,7 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat PDF")
+    st.set_page_config("Chat with PDF")
     st.header("Chat with PDF using Gemini")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
@@ -99,9 +99,9 @@ def main():
         user_input(user_question)
 
     with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
+        st.title("Dashboard:")
+        pdf_docs = st.file_uploader("Upload your PDF Files here", accept_multiple_files=True)
+        if st.button("Submit"):
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
@@ -112,3 +112,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
